0111155b844b78314da65fd07d6a9a28337615ae
Mariaapalacios <mp352@njit.edu>
2025-09-06 14:01:33 -0400

    hw1 done

--- hw1.py
+++ hw1.py
@@ -1,10 +1,11 @@
-
+import requests 
 """
 Maria Angel Palacios Sarmiento
 CS490 - 103
 Homework 1 - Student Info Submission
 """
-import requests 
+
+
 url = "https://student-info-api.netlify.app/.netlify/functions/submit_student_info"
 
 #data that will be sent in JSON format
@@ -13,9 +14,25 @@ data = {
     "first_name": "Maria Angel",
     "last_name": "Palacios Sarmiento",
     "github_username": "mariaangelps",
-    "discord_username": "MariaAngelP",
+    "discord_username": "@MariaAngelP",
     "favorite_cartoon": "Brandy and Mr. Whiskers",
     "favorite_language": "Python",
     "movie_or_game_or_book": "The Age of Adaline",
     "section": "103"
 }
+#Sending the GET request
+parameters = {"UCID": "mp352", "section": "103"}
+response = requests.get(url, params=parameters)
+print("GET:", response.json())
+
+#sending the DELETE request
+parameters = {"UCID": "mp352", "section": "103"}
+response = requests.delete(url, params=parameters)
+print(response.json())
+
+#sending the POST request
+response = requests.post(url, json=data)
+print(response.json())
+
+
+
